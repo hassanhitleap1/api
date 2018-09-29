c1c11dc8b5359bd241591a9cab8110c53b85289a
hasan kiwan <email@example.com>
2018-09-29 10:32:52 -0700

    lesson 46

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 use App\User;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,9 +26,21 @@ $factory->define(App\User::class, function (Faker $faker) {
         'admin'=>  $faker->randomElement([User::REGULER_USER, User::ADMIN_USER]),
     ];
 });
+
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name' =>$faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity'=> $faker->numberBetween([1,10]),
+        'status'=> $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image'=> $faker->randomElement(['1.png', '2.png','3.png']),
+        'seller_id'=>User::all()->random()->id,
     ];
 });
