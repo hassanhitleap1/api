e8ce828ac74a0d7b62ad06a7c5109d991bc07d65
hasan <hassanhitleap1@gmail.com>
2018-09-30 12:42:27 +0300

    lesson 60

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+
+trait ApiResponser{
+
+    private  function successResponce($data,$code){
+        return response()->json($data,$code);
+    }
+    
+    protected function errorResponce($message,$code){
+        return response()->json(['error'=>$message,'code'=>$code]);
+    }
+
+    public function showAll(Collection $collection,$code=200){
+        return $this->successResponce(['data'=>$collection ,'code'=>$code]);
+    }
+
+    public function showOne(Model $model,$code=200){
+        return $this->successResponce(['data'=>$model, 'code'=>$code]);
+    }
+} 
