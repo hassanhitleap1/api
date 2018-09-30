ac7ae5e94ca74b51060e1abf497e4c628c000c2b
hasan <hassanhitleap1@gmail.com>
2018-09-30 09:48:08 +0300

    lesson 50

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UserController extends Controller
 {
@@ -14,18 +15,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users= User::all();
+        return response()->json(['data'=>$users],200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
