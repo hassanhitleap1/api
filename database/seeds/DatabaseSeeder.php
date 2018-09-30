22464bec0d1057df41bb45741256044904e8c723
hasan <hassanhitleap1@gmail.com>
2018-09-30 08:23:19 +0300

    lesson 48

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
+use App\User;
+use App\Category;
+use App\Transaction;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHCKES =1 ');
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $usersQuantity=200;
+        $categoryQuantity=30;
+        $productsQuantity=1000;
+        $transctionsQuantity=1000;
+
+        factory(User::class,$usersQuantity)-create();
+
+        factory(Category::class,$categoryQuantity)-create();
+
+        factory(Product::class,$productsQuantity)-create()->each(function($product){
+            $categories=Category::all()->random(mt_rand(1,5))->plack('id');
+            $product->categories()->attach($categories);
+        });
+
+        factory(Transaction::class,$transctionsQuantity)-create();
     }
 }
