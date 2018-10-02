fc3dc3ad62dbea853a755b76a05e8a327f3942ce
hasan <hassanhitleap1@gmail.com>
2018-10-02 17:00:47 +0300

    lesson 74

---
+++ app/Scopes/SellerScope.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class SellerScope implements Scope {
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model){
+       $builder->with('products');
+    }
+
+  
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function remove(Builder $builder, Model $model){} 
+
+
+    
+}
+?>
