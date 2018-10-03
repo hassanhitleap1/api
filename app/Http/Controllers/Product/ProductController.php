814f733d120661de0cb3f43017107005c331e3fb
hasan <hassanhitleap1@gmail.com>
2018-10-03 16:31:56 +0300

    lesson 83

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -25,9 +25,9 @@ class ProductController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Product $product)
     {
-        //
+        return $this->showOne($product);
     }
 
    
