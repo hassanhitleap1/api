19a8a0f26b81b8fa63ec47b554195e99c15d240d
hasan <hassanhitleap1@gmail.com>
2018-10-04 15:14:49 +0300

    lesson 98

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::resource('products','Product\ProductController',['only'=>['index','show']
 */
 Route::resource('sellers','Seller\SellerController',['only'=>['index','show']]);
 Route::resource('sellers.transactions','Seller\SellerTransactionController',['only'=>['index']]);
+Route::resource('sellers.categories','Seller\SellerCategoryController',['only'=>['index']]);
 
 
 /**
